4af372641e0087a647c83389951c3063c6462892
Zhanna <zkobzaryova@gmail.com>
2017-10-02 16:16:33 +0300

    pin

--- testPosts/pinterest/pinterest.js
+++ testPosts/pinterest/pinterest.js
@@ -1,75 +1,75 @@
 
-function sendRequest(){
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-};
-
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-  return text.match('<title>(.*)?</title>')[1];
-};
-
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // This is a sample server that supports CORS.
-  var username = document.getElementById('username'),
-  url = "https://www.pinterest.com/" + username.value + "/feed.rss/index.xml";
-
-  var xhr = createCORSRequest('GET', url);
-  console.log(this.responseText);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + url + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-};
-};
-
-
-
-
 // function sendRequest(){
-// var x = new XMLHttpRequest(),
-
-//     username = document.getElementById('username'),
-//     url = "https://www.pinterest.com/" + username.value + "/feed.rss/index.xml";
-    
-//     x.open("GET", url, true);
-//     x.onreadystatechange = function () {
-//   if (x.readyState == 4 && x.status == 200)
-//   {
-//     var doc = x.responseXML;
-//     console.log(doc);
+// // Create the XHR object.
+// function createCORSRequest(method, url) {
+//   var xhr = new XMLHttpRequest();
+//   if ("withCredentials" in xhr) {
+//     // XHR for Chrome/Firefox/Opera/Safari.
+//     xhr.open(method, url, true);
+//   } else if (typeof XDomainRequest != "undefined") {
+//     // XDomainRequest for IE.
+//     xhr = new XDomainRequest();
+//     xhr.open(method, url);
+//   } else {
+//     // CORS not supported.
+//     xhr = null;
 //   }
+//   return xhr;
 // };
-// x.send();
 
+// // Helper method to parse the title tag from the response.
+// function getTitle(text) {
+//   return text.match('<title>(.*)?</title>')[1];
 // };
+
+// // Make the actual CORS request.
+// function makeCorsRequest() {
+//   // This is a sample server that supports CORS.
+//   var username = document.getElementById('username'),
+//   url = "https://www.pinterest.com/" + username.value + "/feed.rss/index.xml";
+
+//   var xhr = createCORSRequest('GET', url);
+//   console.log(this.responseText);
+//   if (!xhr) {
+//     alert('CORS not supported');
+//     return;
+//   }
+
+//   // Response handlers.
+//   xhr.onload = function() {
+//     var text = xhr.responseText;
+//     var title = getTitle(text);
+//     alert('Response from CORS request to ' + url + ': ' + title);
+//   };
+
+//   xhr.onerror = function() {
+//     alert('Woops, there was an error making the request.');
+//   };
+
+//   xhr.send();
+// };
+// };
+
+
+
+
+function sendRequest(){
+var x = new XMLHttpRequest(),
+
+    username = document.getElementById('username'),
+    url = "https://www.pinterest.com/" + username.value + "/feed.rss/index.xml";
+    
+    x.open("GET", url, true);
+    x.onreadystatechange = function () {
+  if (x.readyState == 4 && x.status == 200)
+  {
+    var doc = x.responseXML;
+    console.log(doc);
+  }
+};
+x.send();
+
+};
 
 
 
